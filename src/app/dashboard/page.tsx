9f0f3ebcc5fed586fc3c26c74d2fce9e7f4f9e4b
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-05-20 17:20:12 +0800

    Added Chart UI

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
+import { Card } from "@/components/ui/card";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Component } from "@/components/utils-components/Chart";
+
+import { ThermometerSun } from "lucide-react";
 
 export default function Dashboard() {
   return (
@@ -8,7 +12,37 @@ export default function Dashboard() {
       <AppSidebar />
       <SidebarInset>
         <SiteHeader />
-        <div className="h-full w-screen"></div>
+        <div className="font-inter h-full w-full p-15">
+          <Card className="h-full gap-10 bg-white p-10">
+            <div className="flex flex-row gap-5">
+              <Card className="flex h-fit w-[30rem] flex-col gap-1 px-10 py-7">
+                <div className="flex items-center justify-between">
+                  <div className="flex flex-col">
+                    <h1 className="text-black">Temperature</h1>
+                    <small className="text-gray-500">
+                      Updated every 30 secs
+                    </small>
+                  </div>
+                  <ThermometerSun size={20} color="#6A7282" />
+                </div>
+                <h2 className="text-3xl font-bold">45.67</h2>
+              </Card>
+              <Card className="flex h-fit w-[30rem] flex-col gap-1 px-10 py-7">
+                <div className="flex items-center justify-between">
+                  <div className="flex flex-col">
+                    <h1 className="text-black">Humidity</h1>
+                    <small className="text-gray-500">
+                      Updated every 30 secs
+                    </small>
+                  </div>
+                  <ThermometerSun size={20} color="#6A7282" />
+                </div>
+                <h2 className="text-3xl font-bold">68.92</h2>
+              </Card>
+            </div>
+            <Component />
+          </Card>
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
---
+++ src/components/utils-components/Chart.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { TrendingUp } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+export function Component() {
+  return (
+    <Card className="w-[30rem]">
+      <CardHeader>
+        <CardTitle>Area Chart - Stacked</CardTitle>
+        <CardDescription>
+          Showing total visitors for the last 6 months
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+            className="w-[30rem]"
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dot" />}
+            />
+            <Area
+              dataKey="mobile"
+              type="natural"
+              fill="var(--color-chart)"
+              fillOpacity={0.4}
+              stroke="var(--color-chart)"
+              stackId="a"
+            />
+            <defs>
+              <linearGradient id="colorChart" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#fcd34d" stopOpacity={1} />
+                <stop offset="100%" stopColor="#f8fafc" stopOpacity={1} />
+              </linearGradient>
+            </defs>
+            <Area
+              dataKey="desktop"
+              type="natural"
+              fill="url(#colorChart)"
+              fillOpacity={0.4}
+              stroke="#fdd34d"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 leading-none font-medium">
+              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="text-muted-foreground flex items-center gap-2 leading-none">
+              January - June 2024
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
