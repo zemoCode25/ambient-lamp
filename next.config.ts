a0201ae01b82ba9723b0997ccf17e7720ed4267a
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-05-22 17:50:52 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["http://192.168.1.8:3000"],
 };
 
 export default nextConfig;
